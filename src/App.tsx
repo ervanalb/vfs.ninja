d3bf3a27f3afe2f5ba3a1be3c9778747dcaaeba3
Eric Van Albert <eric@van.al>
2025-10-23 15:42:51 -0400

    Split about page into separate route

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,12 @@ import { formations, costMatrix, FormationId, Position, CompClassId, compClasses
 import Form from 'react-bootstrap/Form';
 import Collapse from 'react-bootstrap/Collapse';
 import Modal from 'react-bootstrap/Modal';
-import Figure from 'react-bootstrap/Figure';
 import Button from 'react-bootstrap/Button';
 import rerun from './icons/rerun.svg';
 import close from './icons/close.svg';
 import plus from './icons/plus.svg';
 import about from './icons/about.svg';
 import attentionIcon from './icons/attention.svg';
-import picHIba from './about/h_iba.png';
-import picHP1 from './pics/h_p1.svg';
-import picHP2 from './pics/h_p2.svg';
-import pic8Inter from './pics/8_inter.svg';
-import pic7V1 from './pics/7_v1.svg';
-import pic7Lh from './about/7_lh.svg';
 
 const formationsInCompClass: (compClass: CompClassId) => Array<FormationId> = (compClass) =>
   (Object.entries(formations).filter(([_, { compClasses }]) => compClasses.includes(compClass)).map(([id]) => id))
@@ -1295,157 +1288,6 @@ const deserialize = (
   setNumRounds(draw.length);
 };
 
-const aboutHtml = <>
-  <h4>What is 4-way VFS?</h4>
-  <p>
-    4-way vertical formation skydiving (VFS) is a competitive skydiving discipline where a team of 4 fliers attempts to build a sequence of formations as quickly as possible.
-    The sequence for each round of competition is randomly drawn from a pool of possible formations.
-    The formations dictate the grips and orientations (head-up or head-down.)
-    Teams have 35 seconds to loop through the sequence as many times as possible.
-    The dive pool consists of <em>randoms</em>, which are a single formation,
-    and <em>blocks</em>, which consists of a starting formation,
-    some kind of separation or movement, and an ending formation.
-    Check out <a href="https://www.youtube.com/watch?v=_5YaVhswn8Y">this video</a> to see what it looks like.
-  </p>
-  <p>
-    The <a href="https://www.fai.org/sites/default/files/isc/documents/2024/2024_isc_cr_formation_skydiving_vertical_formation_skydiving.pdf">offical rules</a> are somewhat lenient.
-    For example, they do not specify whether the four fliers should be stretched out into a line, or curled into a circle,
-    as long as the correct grips are taken.
-    The rules also don't specify which flier fulfills which role, and allow the formation to be built mirrored.
-    Since there are no points for style, only speed, it is important to make a plan that that minimizes transitions and movement,
-    a process known as <em>engineering</em>.
-  </p>
-  <Figure>
-    <Figure.Image src={picHIba} width={150} height={150} />
-    <Figure.Image src={picHP1} width={150} height={150} />
-    <Figure.Image src={picHP2} width={150} height={150} />
-    <Figure.Caption>
-      The same formation three different ways (first picture from <a href="https://www.tunnelflight.com/">IBA</a>)
-    </Figure.Caption>
-  </Figure>
-
-  <h4>What is this tool?</h4>
-  <p>
-    <em>vfs.ninja</em> is intended to be a training tool for 4-way VFS teams.
-    Unlike the official rules, whose pictures show the formations in a way that is unambiguous and legal,
-    this tool attempts to visualize the formations in a way that is <em>efficient to build</em>.
-    It has several variations of each formation, and automatically picks one based on the previous formation,
-    in an attempt to optimize the engineering.
-  </p>
-  <p>
-    In some cases, you may disagree with the choice the algorithm made
-    (although if you think something is wrong, please file a <a href="https://github.com/ervanalb/draw-generator/issues">bug report</a>.)
-    By clicking on a formation, you can explore different engineering options.
-    This can be used to communicate engineering choices, to make sure everybody is on the same page.
-    The URL of the page will reflect any edits you make, and can be shared.
-  </p>
-  <p>
-    The approach taken here is largely inspired by <a href="https://www.sdccore.com/">SDC core</a> and their methodology.
-    The engineering is based on a circular arrangement of fliers and fixed cross-partners and piece-partners.
-  </p>
-  <ul>
-    <li>The black flier&apos;s cross partner is green, and piece partner is red. This is the "primary head-up" role.</li>
-    <li>The red flier&apos;s cross partner is blue, and piece partner is black.</li>
-    <li>The green flier&apos;s cross partner is black, and piece partner is blue.</li>
-    <li>The blue flier&apos;s cross partner is red, and piece partner is green. This is the "primary head-down" role.</li>
-  </ul>
-
-  <Figure>
-    <Figure.Image src={pic8Inter} width={150} height={150} />
-    <Figure.Image src={pic8Inter} width={150} height={150} className="slot-switch-transverse" />
-    <Figure.Caption>
-      Regular slots and alternate slots
-    </Figure.Caption>
-  </Figure>
-
-  <p>
-    There is a lot that this tool does not cover. For example:
-  </p>
-  <ul>
-    <li>It does not indicate when grips should be taken early and "flipped."</li>
-    <li>It does not contain variations of a formation that involve partnering up with non-piece-partners
-      if a piece-partner option is available.</li>
-  </ul>
-
-  <p>
-    Many formations can be built left- or right-handed, e.g. 7.
-    The above rule, "use piece partners if possible," tends to force either the left- or right-hand variant,
-    even when the distinction is not particularly meaningful (like in 7.)
-    In other words, this tool chooses to keep piece partners consistent rather than keeping the choice between left- and right-hand consistent.
-    You may want to make a different choice.
-  </p>
-
-  <Figure>
-    <Figure.Image src={pic7V1} width={150} height={150} />
-    <Figure.Image src={pic7Lh} width={150} height={150} />
-    <Figure.Caption>
-      Two flowers (block 7) with opposite handedness.
-      The second option is not included in this tool, because it does not maintain piece partners,
-      even though the distinction is not very meaningful in this case.
-    </Figure.Caption>
-  </Figure>
-
-  <h4>Details for nerds</h4>
-  <p>
-    Each formation variant stores the orientation of each flier (e.g "head up", or "head down outface".)
-    A cost matrix defines how expensive it is to make a given transition (e.g. "head down" to "head up" is more expensive than "head down outface" to "head down.")
-    Using the sum of the four transition costs, a greedy algorithm is used to pick the next formation variant.
-    These computed variants, along with whether fliers are in their regular or alternate slots, are iterated forward until a cycle is found.
-    Alternate slots are visualized by mirroring the picture.
-  </p>
-
-  <h4>Changelog</h4>
-  <h5>v2.0</h5>
-  <em>2025-XX-XX</em>
-  <ul>
-    <li>Add piece-partner variant of block 10</li>
-    <li>Rename piece partner variants of random Q</li>
-    <li>Add ability to manually enter a draw</li>
-    <li>Introduce additional modifiers: slot switchers, uncommon roles, uncommon shapes</li>
-    <li>Fix browser navigation (back / forward button should work more reliably)</li>
-    <li>Change optimizer behavior slightly</li>
-    <li>Minor fixes to random selection process</li>
-  </ul>
-  <h5>v1.2</h5>
-  <em>2025-03-01</em>
-  <ul>
-    <li>Draw formations without replacement (until the pool is exhausted)</li>
-    <li>Update block 12 as per <a href="https://www.fai.org/sites/default/files/isc/ibd/2024/ibd_2024-14_vfs_dive_pool_change_for_2025_with_inmediate_effect.pdf">IBD 2024-14</a></li>
-    <li>Add missing variants for block 2</li>
-    <li>Adjust some pictures for improved clarity (2, F, J)</li>
-    <li>Remove scrollbars on individual rounds for better experience on mobile</li>
-  </ul>
-  <h5>v1.1</h5>
-  <em>2025-01-03</em>
-  <ul>
-    <li>Fix how optimizer handles block 9 (so it becomes a switcher when combined with blocks like 4)</li>
-  </ul>
-  <h5>v1.0</h5>
-  <em>2024-12-29</em>
-  <ul>
-    <li>Initial release</li>
-  </ul>
-
-  <h4>Disclaimer / License</h4>
-  <p>
-    Every effort has been made to ensure the pictures and algorithms are in accordance with the official rules,
-    but accuracy is not guaranteed.
-  </p>
-  <p>
-    The software is licensed under the <a href="https://opensource.org/license/mit">MIT</a> license.
-  </p>
-  <p>
-    The pictures are licensed under the <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.en">CC BY-SA 4.0</a> license.
-  </p>
-  <p>
-    More information is available on <a href="https://github.com/ervanalb/draw-generator">github</a>.
-    Bug reports and contributions are welcome.
-  </p>
-  <p>
-    <em>~ervanalb</em>
-  </p>
-</>;
-
 const App = () => {
   // Setup
   const [compClass, setCompClass] = useState<CompClassId | "custom">(initialCompClass);
@@ -1461,7 +1303,6 @@ const App = () => {
   const [hash, setHash] = useState<string | null>(null);
   const [deserializeTrigger, setDeserializeTrigger] = useState<boolean>(false);
   const [newHistoryEntry, setNewHistoryEntry] = useState<boolean>(false);
-  const [aboutShown, setAboutShown] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [manualEntryShown, setManualEntryShown] = useState<boolean>(false);
   const [manualEntry, setManualEntry] = useState<string>("");
@@ -1915,12 +1756,11 @@ const App = () => {
   </Form.Group>;
 
   return <>
-    <h1>vfs.ninja</h1>
     <div className="title-container">
-      <h2>4-way VFS draw generator</h2>
-      <button className="about-button" onClick={() => { setAboutShown(true); }}>
+      <h1>vfs.ninja - draw generator</h1>
+      <a href="/about">
         <img src={about} />
-      </button>
+      </a>
     </div>
     <h3>Setup</h3>
     <Setup compClass={compClass} setCompClass={setCompClass}
@@ -1996,12 +1836,6 @@ const App = () => {
       changeEngineering={changeEngineering}
       includedFormations={includedFormations}
     />
-    <Modal show={aboutShown} onHide={() => { setAboutShown(false); }}>
-      <Modal.Header closeButton><Modal.Title>About</Modal.Title></Modal.Header>
-      <Modal.Body>
-        {aboutHtml}
-      </Modal.Body>
-    </Modal>
     <Modal show={manualEntryShown} onHide={() => { setManualEntryShown(false); }}>
       <Modal.Header closeButton><Modal.Title>Manual Entry</Modal.Title></Modal.Header>
       <Modal.Body>
@@ -2044,4 +1878,4 @@ const App = () => {
   </>
 };
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router";
 import './App.css';
 import App from './App.tsx'
+import About from './About.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode >
+);
